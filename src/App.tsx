98355bb9342e33d19139065dcc00dd932ef923b5
Yash Sehgal <yashsehgal.work@gmail.com>
2023-09-27 16:00:30 +0530

    feat: add boilerplate text

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@
 const App: React.FunctionComponent = () => {
   return (
     <div className="App">
-      <h2 className="my-12 mx-24 leading-snug text-xl font-medium">
-        Pair programming <br />assignment
+      <h2 className="sepia my-12 mx-24 leading-snug text-xl font-medium border-4 rounded-md border-neutral-800 p-2 w-fit h-auto">
+        Pair<br />
+        programming<br />
+        round
       </h2>
     </div>
   )
